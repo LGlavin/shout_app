282199990944e7f7cba7057ca91bec320660bca0
Elizabeth Glavin <eglavin@gmail.com>
2013-09-08 20:08:19 -0400

    add user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shout::Application.config.secret_key_base = 'afab2016b61ec972e5d928c5ae82f49158863a6e9e157767d162aa0183ca0099d2e3431f9549d1ae72c3d91bd8361a3ea6a2d543d4fba3447b8087f35693e4b3'
